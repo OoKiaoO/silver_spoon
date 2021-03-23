1b425c9330eb6077246fe7859eab664f1be387e4
Chiara Campalani <campalanichiara@gmail.com>
2021-03-23 14:25:36 +0900

    created searchbar component

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,6 +3,8 @@ import Business from '../Business/Business';
 
 import './BusinessList.css';
 
+import './BusinessList.css';
+
 class BusinessList extends React.Component {
   render() {
     return (
