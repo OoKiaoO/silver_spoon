96bc1d783cf3f9ee686ff2958430977d247f2153
Chiara Campalani <campalanichiara@gmail.com>
2021-03-28 11:23:27 +0900

    refactored Business & BusinessList components

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,18 +3,13 @@ import Business from '../Business/Business';
 
 import './BusinessList.css';
 
-import './BusinessList.css';
-
 class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
+        {this.props.businesses.map((business) => {
+          return <Business business={business} />;
+        })}
       </div>
     )
   }
